902c1856c703d2d0715d80f4d02d241d291b57a4
dheeraj0007 <dheeraj1863.be20@chitkara.edu.in>
2025-08-25 21:51:41 +0530

    another try

--- app/page.tsx
+++ app/page.tsx
@@ -29,7 +29,7 @@ export default function Home() {
         {/* Background video */}
         <video
           className="absolute top-0 left-0 w-full h-full object-cover -z-10"
-          src="/header.mov"
+          src="/header.MOV"
           autoPlay
           loop
           muted
